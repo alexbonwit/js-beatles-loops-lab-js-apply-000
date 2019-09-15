36715fa528ad5ca53e9e2b68ddab2fb941c6ef98
candescent-media-9936 <alexbonwit@mac.com>
2019-09-15 00:54:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ function theBeatlesPlay(musicians, instruments) {
   for (var i = 0; i < musicians.length; i++) {
     beatlesArray.push(musicians[i] +' plays ' + instruments[i]);
   }
+  return beatlesArray;
 }
